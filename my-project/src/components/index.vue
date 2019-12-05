7493ba5625bf365790bf828550b7775c47f2f463
e <HolyTracy>
2019-12-05 11:29:06 +0800

    增加背景音乐

--- my-project/src/components/index.vue
+++ my-project/src/components/index.vue
@@ -1,12 +1,21 @@
 <template>
     <div>
         <el-row :gutter="20">
-            <el-col :span="12"><div>1</div></el-col>
-            <el-col :span="12"><div>2</div></el-col>
+            <el-col :span="12">
+                1212
+                <audio src="/static/奇妙能力歌-田馥甄.mp3" autoplay loop></audio>
+            </el-col>
+            <el-col :span="12">
+                <div>2</div>
+            </el-col>
         </el-row>
         <el-row :gutter="20">
-            <el-col :span="12"><div>1</div></el-col>
-            <el-col :span="12"><div>2</div></el-col>
+            <el-col :span="12">
+                <div>1</div>
+            </el-col>
+            <el-col :span="12">
+                <div>2</div>
+            </el-col>
         </el-row>
     </div>
 </template>
